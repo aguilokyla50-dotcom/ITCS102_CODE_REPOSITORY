b5385a2dc992805b1b8fe81c9b9fed85a37279e0
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-08-18 15:43:07 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,37 @@
+# integer variable
+
+shoeSize = 24
+myHeight = 45
+cat = 1
+
+# float variables
+
+myGrade = 94.4
+myTemperature = 36.5
+distance = 14.5
+
+# string variables
+
+myName = "Kyla Aguilo"
+course = "Bachelor of Science in Information Technology"
+school = "Dalubhasaan ng Lungsod ng Lucena"
+
+# boolean variable
+
+isStudent = True
+isHappy = True
+isFinished = True
+
+
+print("Hi! My Name is ", myName)
+print ("currently studying at", school)
+print ("My course is ",course, "and my GWA way back when I was in grade 12 is ", myGrade, "percent")
+print ("The distance I travel going to school is",distance, "km.") 
+
+print("My height is", myHeight, "cm, while my shoe size is", shoeSize, "centimeters and my temperature is", myTemperature, "degree celcsius")
+
+print ("I am a", isStudent, "student with only ", cat,"pet cat. It is ",isHappy," that Iam happy, and it is ", isFinished,"that i did finished my works.")
+
+
+
+
