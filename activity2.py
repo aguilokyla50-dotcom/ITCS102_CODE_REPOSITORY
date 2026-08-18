ddbd4a5e7cc237fc1842cecb2dd31ec82d0c3352
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-08-18 09:05:10 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,6 @@
+# comments -informational  message
+# ignore by python 
+# this is my very first programming code EVER
+
+print(" Hello World and Hello Philippines")
+print(" Hello World ILOVE TO CODE")
