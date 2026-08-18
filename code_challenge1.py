72f07536b466596b3f5f0b0a4cd762b88660a635
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-08-18 09:04:14 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,12 @@
+name= '\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t'
+name2='\t\n\n\t\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t'
+food='\t\n\n\t\t\t\t\t\t*\t\t\t\t*\t\t\t\t\t\t'
+food2='\t\n\n\t\t\t\t\t*\t          Hi,My name is\t\t\t*\t\t\t\t\t\t'
+age='\t\n\n\t\t\t\t\t\t*\t  "Kyla Aguilo" \t*\t\t\t\t\t\t\t\t'
+age2='\t\n\n\t\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t'
+section='\t\n\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t'
+
+
+
+
+print(name,name2,food,food2,age,age2,section)
