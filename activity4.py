519a8b38341d5b5eae75ac1ed10389106b358202
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-08-23 14:16:43 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,13 @@
+# scape sequence
+# is used to modify in line strings 
+# \n -new line
+# \t - tab space
+# \r - carriage return
+# \\ - insert slash
+# \* - insert double quote
+# \b - back space
+# \ is diffirent from /
+
+motto2 = "The Quick \tBrown Fox \nJumps over \rthe Lazy Dog.\nA negative \tmind would \tnever\b\b\b\b\b give you a\bpositive life"
+                       
+print(motto2)
