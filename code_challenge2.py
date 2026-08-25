214a2bae1b1243963e4c7a52b8e2a49fb5feead6
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-08-25 15:49:21 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,23 @@
+money = 6897
+
+
+a =  1000
+b = 500
+c = 200
+d = 100
+e = 50 
+f = 20
+g = 10
+h = 5 
+i = 1
+
+print("money to deposite-->",money)
+print("1000 =",((6*a)/a))
+print("500 =",((1*a)/a))
+print("200 =",((1*a)/a))
+print("100 =",((1*a)/a))
+print("50 =",((1*a)/a))
+print("20 =",((2*a)/a))
+print("10 =",((0*a)/a))
+print("5 =",((1*a)/a))
+print("1=",((2*a)/a))
