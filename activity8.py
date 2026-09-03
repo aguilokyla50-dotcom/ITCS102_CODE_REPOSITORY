63b58325d59a0855f1d7a3d55dbe384c6373f1df
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-03 19:26:02 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,16 @@
+# concatenation = combining strings
+
+hobbies = "" # blank strings
+
+h = input("What are your hobbies--->")
+hobbies += h + ","
+
+h = input("What else--->")
+hobbies += h + ","
+
+h = input("What else--->")
+hobbies += h + "."
+
+
+print ("So my hobbies are ", hobbies)
+
