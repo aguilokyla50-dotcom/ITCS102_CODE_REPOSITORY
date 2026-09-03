7a680418b08a2f22f971f95d675b810caac9913c
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-03 19:25:40 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,36 @@
+total = 0 
+
+n1 = eval(input("Enter a number--->"))
+total = total + n1
+
+n2 = eval(input("Enter a number--->"))
+total = total + n2
+
+n3 = eval(input("Enter a number--->"))
+total = total * n3
+
+n4 = eval(input("Enter a number--->"))
+total = total - n4
+
+n6 = eval(input("Enter a number--->"))
+total = total / n6
+
+
+n7 = eval(input("Enter a number--->"))
+total = total + n7
+
+n8 = eval(input("Enter a number--->"))
+total = total * n8
+
+n9 = eval(input("Enter a number--->"))
+total = total + n9
+
+n5 = eval(input("Enter a number--->"))
+total = total + n5
+
+n10 = eval(input("Enter a number--->"))
+total = total - n10
+
+
+print("The total is", total)
+
