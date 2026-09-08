07aa75835af4c0236601f11cf124b87112f8e31f
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-08 12:42:00 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,24 @@
+#relational operator demo
+
+x = 15
+y = 9
+
+user1 = "Sample"
+user2 = "sample"
+
+#1
+print( x > y)
+#2
+print( x < y)
+#3
+print( x >= y)
+#4
+print( x <= y)
+#5
+print(x == y)
+#6
+print(user1 == user2)
+#7
+print(user1 != user2)
+#8
+print(x != y)
