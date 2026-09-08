46d0bfaa6cdfd14996974625b00f9d916e6f883a
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-08 12:30:34 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,18 @@
+a = 10 
+b = 5
+c =7
+
+
+print(a > b or c < a)
+
+print(a > b or c < a and c ==b)
+
+print(not ( a > b and  c < a or c == b ))
+
+
+print(not ( a > b or  c < a and  c == b ))
+
+
+
+
+
