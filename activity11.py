50bc4dab93cd496bcc46cdacef03618048339623
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-08 12:42:36 +0800

    Add files via upload

--- activity11.py
+++ activity11.py
@@ -2,9 +2,9 @@ username = "kyla05"
 password = 'SECRETHAHAHA'
 
 u = input("Enter username -->")
-p = input("Enter password ==>")
+p = input("Input password ==>")
 
 if username == u and password == p :
 	print("ACCESS GRANTED")
 else:
-	print("ACCESS DINIED")
+	print("ACCESS DENIED")
