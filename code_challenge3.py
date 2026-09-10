0ca01c2078a854c7232966dc30947e642c19ae26
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-10 12:49:36 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,19 @@
+Sender = input("What is your name? = ")
+
+Type_of_Item = input("What type of item did you order? = ")
+
+isfragile = bool(input(" Is your order fragile? = "))
+
+
+if isfragile == True:
+	print(" be careful baka mahulog")
+
+else:
+	print("edi ikaw na ang taugh")
+
+Weight = float(input("How heavy(KG) is your order? = "))
+
+distance = float(input(" how far is the destination?"))
+
+
+
