49e222aa8173d1ad701e9941e61282a343470e55
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-11 14:43:23 +0800

    Add files via upload

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,39 +1,40 @@
 Sender_Name = input("Name of Sender? --> ")
-Type_of_Item = input("What type of Item did you order? ---> ")
-Is_Fragile = bool(input("Is your order Fragile? (Enter \"YES\" if True and press Enter if False) ---> "))
-weight = float(input("How heavy is the Weight of the object? (in kg) ---> "))
-distance = float(input("How far is the destination(In km) ---- > "))
-is_express = bool(input("Express? (Enter \"YES\" if True and press Enter if False) --> "))
-is_international = bool(input("International? (Enter \"YES\" if True and  press Enter if False) --> "))
 
+Type_of_Item = input("What type of Item did you order? ---> ")
+
+Is_Fragile = bool(input("Is your order Fragile? (Enter \"yes\" if yes, eneter \"no\" if no) --->") == "yes")
+
+weight = float(input("How heavy is the Weight of the object? (in kg) ---> "))
+
+distance = float(input("How far is the destination(In km) ---- > "))
+
+is_express = bool(input("Express? (Enter \"yes\" if yes, eneter \"no\" if no) --->")== "yes")
+
+is_international = bool(input("International? (Enter \"yes\" if yes, eneter \"no\" if no) --->")== "yes")
 
 
 # Calculating base cost
 
-base_cost = (weight * 2.5) + (distance * .15)
+base_cost = (weight * 2.50) + (distance * 0.15)
 
-#Free shipping
-
-
-if  weight <= 2 and distance <= 100 and is_express == False and is_international == False :
-	print("Free Shipping!!")
-	Total = 0
-	
 
 #International Express
 
-elif is_international == True and is_express == True :
-	print("Package is International is applied")
+
+
+if weight <= 2  and distance <=100 and not is_international:
+	print("free shipping!!!")
+	total = 0
+
+elif is_international and is_express:
+	print("International express is applied")
 	Total = (base_cost * 1.4) + 50
 
-#Express or Heavy International
-
-elif is_express == True or (is_international == True and weight > 20) : 
+elif (is_express or is_international) and weight > 20: 
 	print("Package is Express or Heavy International is applied")
 	Total = (base_cost * 1.2) + 25
 
-#Oversized
-elif weight > 30 or distance > 1000 : 
+elif weight > 30 or distance > 1000: 
 	print("Oversized is applied")
 	Total = base_cost + 30
 #Standard rate
@@ -42,10 +43,10 @@ else :
 	Total = base_cost
 	print("Standard rate is applied")
 
-ShippingFee = Total - base_cost
+ShippingFee = total - base_cost
 
 
-print("------------------------------")
+print("=================================")
 print("KYLA'S DELIVERING COMPANY")
 print("Name: ", Sender_Name)
 print("ITEM: ", Type_of_Item)
@@ -55,8 +56,11 @@ print("FRAGILE???: ",Is_Fragile)
 print("INTERNATIONAL?: ", is_international)
 print("EXPRESS?: ", is_express)
 print("TOTAL SHIPPING COST: PHP ", ShippingFee)
-print("Total: PHP ", Total)
-print("-------------------------------")
+print("Total: PHP ", total)
+print("================================")
+
+print("Thank you so much for purchasing from us! We truly appreciate your support. We hope you love your purchase!")
+("Thank you for choosing our small business!")
 
 
 
