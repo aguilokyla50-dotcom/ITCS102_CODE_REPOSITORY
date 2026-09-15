5ad55f541c7d7f38cfd8b4d049f328510b8ce2fd
aguilokyla50-dotcom <aguilokyla50@gmail.com>
2026-09-15 12:54:30 +0800

    Add files via upload

---
+++ activity14.py
@@ -0,0 +1,21 @@
+age = int(input("Enter your age---->"))
+is_employed = bool(input("Are you currently employed(True/Flase)"))
+credit_score = int(input("what is your credit sccore?--->"))
+annual_income = float(input("what is your anual income?--->"))
+has_collateral = bool(input("Do you have any collateral (True/False"))
+
+    if age >= 21 and is_employed == True:
+        pass
+    else
+        pass
+    
+print("applicant pass the baseline requirement")
+    if credit_score >= 750:
+        pass
+    else
+        pass
+
+print("you have a high credit score")
+print ("be;ow tier 1 credit_score")
+
+    else:print(" rejected: faild baseline criteria")
